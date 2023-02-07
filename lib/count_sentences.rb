bf784a4195b535fc4898a0f15507301e7cc2c8cc
Mordechai <mordwein77@gmail.com>
2023-02-06 19:27:25 -0500

    commit tree 213a7daf123d1219255bbda52e2d74da8fcd7d1c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+   number_array = self.split(/[.?!]/).reject {|string| string.empty?}
+   number_array.length
   end
 end
+
